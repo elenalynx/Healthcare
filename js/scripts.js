bdff1c1a8a8ae541854eeb232eee7ca480c97c04
elenalynx <elnlynx@gmail.com>
2022-09-19 04:02:27 +0300

    added section Easy Steps, quality service, Our Service (carousel), part Meet Doctors (card)

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,65 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
+        loop: true,
+        dots: true,
+        dotsEach: true,
+        margin: 65,
+        center: true,
+        // items: 3,
+        // slideBy: 1,
 
         autoplay: true,
         autoplayTimeout: 2000,
 
-        nav:true,
-        navText: [ '', ' ' ],
+        nav: true,
+        navText: ['', ' '],
 
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
 
-            1000:{
-                items:1
-            }
+            767: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
         }
+    });
+
+    $('.step-slider').on('changed.owl.carousel', function (event) {
+        var idx = event.item.index; // номер активного элемента
+    });
+
+
+    $(function () {
+
+        $('.load-more').on('click', function () {
+            const btn = $(this);
+            const loader = btn.find('span');
+            $.ajax({
+                url: '/data.html',
+                type: 'GET',
+                beforeSend: function () {
+                    btn.attr('disabled', true),
+                        loader.addClass('d-inline-block');
+                },
+                success: function (responce) {
+                    setTimeout(function () {
+                        loader.removeClass('d-inline-block');
+                        btn.attr('disabled', false);
+                        // console.log(responce);
+                    $('.after-posts').before(responce);
+                    }, 1000);
+                },
+                error: function () {
+                    alert('Error!');
+                    loader.removeClass('d-inline-block');
+                    btn.attr('disabled', false);
+                }
+            });
+        })
     });
 });
